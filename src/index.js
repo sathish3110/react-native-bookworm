1a69dfc77b69a05db12a33ad64051271c3c76a87
sathish3110 <sathish.kumar@harnstech.com>
2025-04-03 15:55:10 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,117 @@
+import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  return token;
+};
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // Validate input data
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+      return res
+        .status(400)
+        .json({ message: "Username can only contain alphanumeric characters" });
+    }
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username must be between 3 and 20 characters long" });
+    }
+    // Check if user already exists
+    const existingEmail = User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const existingUsername = User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    // Create new user
+
+    const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`;
+    const user = new User({
+      username,
+      email,
+      password, // In a real application, you should hash the password before saving it
+      profileImage,
+    });
+    await user.save();
+    const token = generateToken(user._id);
+    res.status(201).json({
+      message: "User registered successfully",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Validate input data
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    // user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    // Check password
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    // Generate token
+    const token = generateToken(user._id);
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
